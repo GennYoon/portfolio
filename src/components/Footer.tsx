633067d6a4ca0240becc90a708f0609d16b862cb
GennYoon <yoonwonyoul@webchemist.net>
2024-08-16 01:07:46 +0900

    remove old files

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer className="flex justify-center ms-auto me-auto md:w-[740px] sm:w-full p-4">
-      <p className="text-gray-400">© 2024 GennYonn. All Rights Reserved.</p>
-    </footer>
-  );
-};
