7728832ff1863ae69ae2cfe4bc980d875913a828
GennYoon <yoonwonyoul@webchemist.net>
2024-08-16 01:31:57 +0900

    vercel, nextjs build시에 supabase 폴더 제외 추가

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,5 +4,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
