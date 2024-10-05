5e51bc1d809b1b3ccef78bc530acffcd19231cce
GennYoon <yoonwonyoul@webchemist.net>
2024-10-05 16:25:13 +0900

    Release 0.2.0 (#4)
    * feat: modify main page
    
    * chore: release v0.2.0

--- src/components/widget/header.tsx
+++ src/components/widget/header.tsx
@@ -1,10 +1,22 @@
 import ModeWidget from "@/components/widget/mode";
 
+import { Button } from "@/components/ui/button";
+
+import { ChevronsLeftRightEllipsis } from "lucide-react";
+
 const HeaderWidget = () => {
   return (
     <header className="fixed left-0 right-0 top-0 z-[2] bg-[#dfdfdf30] backdrop-blur-[10px] dark:bg-[#20202320]">
-      <div className="ml-auto mr-auto flex items-center justify-between px-4 py-2 sm:w-full md:w-[768px]">
+      <div className="ml-auto mr-auto flex items-center justify-between gap-2 px-4 py-2 sm:w-full md:w-[768px]">
         <h1 className="text-xl font-bold">GennYoon Portfolio</h1>
+        <div className="flex flex-1 justify-end gap-2">
+          <Button size="icon" variant="outline" asChild>
+            <a href="https://gennyoon.net">
+              <ChevronsLeftRightEllipsis />
+            </a>
+          </Button>
+        </div>
+
         <ModeWidget />
       </div>
     </header>
--- src/components/widget/hero.tsx
+++ src/components/widget/hero.tsx
@@ -4,14 +4,12 @@ import React from "react";
 import { TypeAnimation } from "react-type-animation";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 
 import avatarImg from "@/assets/images/avatar.jpg";
 
-import { GitHubLogoIcon, HomeIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 import { motion, useInView } from "framer-motion";
 
-const HeroSeciton = () => {
+const HeroWidget = () => {
   const ref = React.useRef(null);
   const isInView = useInView(ref, { once: true });
 
@@ -19,14 +17,8 @@ const HeroSeciton = () => {
     initial: { scale: 0.8, opacity: 0 },
     animate: { scale: 1, opacity: 1 },
   };
-
   return (
-    <motion.section
-      ref={ref}
-      variants={canVariants}
-      initial="initial"
-      animate={isInView ? "animate" : "initial"}
-      className="mb-16">
+    <motion.section ref={ref} variants={canVariants} initial="initial" animate={isInView ? "animate" : "initial"}>
       <div className="mt-12 flex justify-between">
         <div>
           <h1 className="bg-gradient-to-r from-[#036CDA] to-[#15F5FD] bg-clip-text text-4xl font-bold text-transparent">
@@ -48,28 +40,8 @@ const HeroSeciton = () => {
           </Avatar>
         </div>
       </div>
-      <div className="flex justify-start gap-1.5">
-        <Button asChild variant="secondary" size="sm" className="rounded-full">
-          <a href="#contract">Contact me here</a>
-        </Button>
-        <Button asChild variant="outline" size="icon" className="rounded-full">
-          <a href="https://gennyoon.net" target="_blank">
-            <HomeIcon />
-          </a>
-        </Button>
-        <Button asChild variant="outline" size="icon" className="rounded-full">
-          <a href="https://www.linkedin.com/in/gennyoon/" target="_blank">
-            <LinkedInLogoIcon />
-          </a>
-        </Button>
-        <Button asChild variant="outline" size="icon" className="rounded-full">
-          <a href="https://github.com/gennyoon" target="_blank">
-            <GitHubLogoIcon />
-          </a>
-        </Button>
-      </div>
     </motion.section>
   );
 };
 
-export default HeroSeciton;
+export default HeroWidget;
